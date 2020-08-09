b717549a2d450683757bb44e3d582552ddcc9d9f
dtischler <{ID}+{username}@users.noreply.github.com>
2020-08-09 09:02:15 -0700

    Removed 'noisy' container

--- noisy/index.js
+++
@@ -1,7 +0,0 @@
-async function makeNoise() {
-  console.log("Hello from", process.env.BALENA_SERVICE_NAME);
-  // Sleep up to 3 seconds
-  setTimeout(makeNoise, Math.floor(Math.random() * Math.floor(3000)));
-}
-
-makeNoise();
